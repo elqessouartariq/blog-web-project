eddfc06760cee3f04ef9a3a5534254a36f7373ac
elqessouartariq <tariqelqessouar@gmail.com>
2023-05-20 19:33:57 +0100

    feat: validate user registration  + signin inputs

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,11 +1,35 @@
 const userService = require('../services/userService');
+const {
+	registerValidationSchema,
+	signinValidationSchema,
+} = require('../validators/userValidator');
 
 const register = async (req, res) => {
+	const { error } = registerValidationSchema.validate(req.body, {
+		abortEarly: false,
+	});
+
+	if (error) {
+		res.status(400);
+		res.send(error.details);
+		return;
+	}
+
 	const token = await userService.register(req.body);
 	res.send({ token });
 };
 
 const signin = async (req, res) => {
+	const { error } = signinValidationSchema.validate(req.body, {
+		abortEarly: false,
+	});
+
+	if (error) {
+		res.status(400);
+		res.send(error.details);
+		return;
+	}
+
 	const token = await userService.signin(req.body);
 	if (!token) {
 		res.status(401);

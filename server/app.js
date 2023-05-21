9ec068aab17917b9866e001ecf84f1da63cb5e4e
elqessouartariq <tariqelqessouar@gmail.com>
2023-05-21 18:43:22 +0100

    feat: Add CORS  for client-server communication

--- server/app.js
+++ server/app.js
@@ -1,3 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const { protect } = require('./modules/auth');
+const cors = require('cors');
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -16,6 +21,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(
+	cors({
+		origin: 'http://127.0.0.1:5173',
+	})
+); // CORS
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -25,23 +35,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/articles', articlesRouter);
-app.use('/comments', commentsRouter);
-app.use('/categories', categoriesRouter);
+app.use('/comments', protect, commentsRouter);
+app.use('/categories', protect, categoriesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
 	next(createError(404));
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function (err, req, res, next) {
+// 	// set locals, only providing error in development
+// 	res.locals.message = err.message;
+// 	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
-});
+// 	// render the error page
+// 	res.status(err.status || 500);
+// 	res.render('error');
+// });
 
 module.exports = app;

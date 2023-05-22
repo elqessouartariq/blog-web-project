f2499e9be84e5b57668517cfd6d1f5074fd7e3a3
elqessouartariq <tariqelqessouar@gmail.com>
2023-05-22 02:05:21 +0100

    feat: Add font families + extended colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			heading: ['Libre Baskerville', 'serif'],
+			primary: ['Open Sans', 'sans-serif'],
+		},
+		extend: {
+			colors: {
+				primaryBlack: '#000000',
+				primaryGray: 'rgba(28,28,28,0.5)',
+				primaryWhite: '#FFFFFF',
+				secondaryBlack: '#1C1C1C',
+			},
+		},
 	},
 	plugins: [],
 };

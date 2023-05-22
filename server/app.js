ed1b0cf35f01d971aefa1960de0482128704d694
elqessouartariq <tariqelqessouar@gmail.com>
2023-05-22 11:09:33 +0100

    feat: protect articles route

--- server/app.js
+++ server/app.js
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/articles', articlesRouter);
+app.use('/articles', protect, articlesRouter);
 app.use('/comments', protect, commentsRouter);
 app.use('/categories', protect, categoriesRouter);
 

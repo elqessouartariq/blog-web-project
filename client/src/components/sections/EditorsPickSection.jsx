6430f00c14601d6ee0a6c552280bfedf7c8a5403
elqessouartariq <tariqelqessouar@gmail.com>
2023-05-22 11:08:39 +0100

    feat: Add Editor's Pick Section

---
+++ client/src/components/sections/EditorsPickSection.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+
+const EditorsPickSection = ({ children }) => {
+	return (
+		<section className="mt-10 flex justify-center flex-wrap">
+			<div className="w-full flex-grow-1 mb-16">
+				<div className="flex items-center justify-center h-full">
+					<div className="text-center flex flex-col items-center">
+						<h1 className="text-5xl font-bold font-primary">
+							Editor Pick
+						</h1>
+						<span className="mt-2">
+							<img src="/src/assets/line.svg" />
+						</span>
+					</div>
+				</div>
+			</div>
+			{children}
+		</section>
+	);
+};
+
+EditorsPickSection.propTypes = {
+	children: PropTypes.node.isRequired,
+};
+
+export default EditorsPickSection;

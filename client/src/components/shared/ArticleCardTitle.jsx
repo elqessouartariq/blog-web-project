ffa01c3d2833651d76cb89d7800c01221ec84287
elqessouartariq <tariqelqessouar@gmail.com>
2023-05-23 19:14:53 +0100

    refactor: add leading

--- client/src/components/shared/ArticleCardTitle.jsx
+++ client/src/components/shared/ArticleCardTitle.jsx
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
 
-const ArticleCardTitle = ({ title }) => {
+const ArticleCardTitle = ({ title, textSize }) => {
 	return (
-		<h3 className="font-heading text-3xl text-primaryBlack font-bold">
+		<h3
+			className={`font-heading ${textSize} text-primaryBlack font-bold leading-snug`}
+		>
 			{title}
 		</h3>
 	);
@@ -10,10 +12,12 @@ const ArticleCardTitle = ({ title }) => {
 
 ArticleCardTitle.propTypes = {
 	title: PropTypes.string,
+	textSize: PropTypes.string,
 };
 
 ArticleCardTitle.defaultProps = {
 	title: 'World’s Most Dangerous Technology Ever Made.',
+	textSize: 'text-3xl',
 };
 
 export default ArticleCardTitle;
